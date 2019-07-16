1f6382c021f75e1c41734d0c4cd73fe7e2d07fbe
Steve Roma <sjroma@hotmail.com>
2019-07-16 18:19:51 -0400

    add remaining, completed course work scripts

--- fccBasicAlgorithmScripting/11titleCaseSentence.js
+++ fccBasicAlgorithmScripting/11titleCaseSentence.js
@@ -4,15 +4,14 @@
 //For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
 function titleCase(str) {
-  return str
-    .toLowerCase()
-    .split(' ')
-    .map(function (eachWord) {
-      return eachWord[0].toUpperCase() + eachWord.substr(1);
-    })
-    .join(' ');
+  let words = str.toLowerCase().split(' ');
+  let capitalWords = [];
+  for (let i = 0; i < words.length; i++) {
+    capitalWords.push(words[i].charAt(0).toUpperCase() + words[i].slice(1));
+  }
+  return capitalWords.join(' ');
 }
 
-//console.log(titleCase("I'm a little tea pot")); //should return I'm A Little Tea Pot
-console.log(titleCase("sHoRt AnD sToUt")); //should return Short And Stout
-//console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT")); //should return Here Is My Handle Here Is My Spout
+//console.log(titleCase("I'm a little tea pot"));                //should return I'm A Little Tea Pot
+console.log(titleCase("sHoRt AnD sToUt"));                       //should return Short And Stout
+//console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));  //should return Here Is My Handle Here Is My Spout
